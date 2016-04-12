efc594f1601d45bf42046fa918da944a67f9fcb4
thematkinson <mark.atkinson@nomad.za.com>
2016-04-12 10:55:11 +0200

    great

---
+++ SPC Package Generator/SPC Package Generator/ConsolidateInstrumentBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace SPC_Package_Generator
+{
+    class ConsolidateInstrumentBuilder
+    {
+
+        public void ConstructInstrumentTask(DataTable columnsDT)
+        {
+            string finalTask = "";
+        }
+
+        private string CreateTempInstTable(DataTable columnsDT)
+        {
+
+            SqlTableStrings sts = new SqlTableStrings();
+            string tempTable = sts.GenerateCreateTable("temp", "newInstrument",columnsDT);
+
+            string insertStatement = CreateInsertStatement(columnsDT);
+
+            string final = String.Concat(tempTable,
+                                         Environment.NewLine,
+                                         insertStatement
+
+                                        );
+
+            return final;
+        }
+
+        private string CreateInsertStatement(DataTable columnsDT)
+        {
+
+            return "";
+        }
+    }
+}
---
+++ SPC Package Generator/SPC Package Generator/ConsolidateInstrumentStrings.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPC_Package_Generator
+{
+    class ConsolidateInstrumentStrings
+    {
+        public static string consolInsPackageStart = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<!--
+/**********************************************************************/
+ *                                                                    *
+ *               Symmetrix DataStage Package File                     *
+ *                 Copyright (c) StatPro                              *
+ *                                                                    *
+ * Change Control:                                                    *
+ * Date (yyyy-MM-dd) Who (Init) Comment                               *
+ *====================================================================*
+ *                                                                    *
+/**********************************************************************/
+-->
+<?xml-stylesheet type=""text/xsl"" href=""packages/report.xslt""?>
+<configuration>
+
+  <Package version=""1.0""
+           name=""ConsolidateInstrument""
+           description=""""
+           log-level=""All"">
+
+    <Variables />
+
+    <Loggers />
+
+    <Connections />
+
+    <Tasks>
+
+      <!-- add tasks here -->" + Environment.NewLine
+                       + Environment.NewLine;
+
+
+
+        public static string consolInsPackageInstrumentTaskStart = @"      <Task name=""Instrument""
+            description=""""
+            type=""Symmetrix.DataStage.Tasks.ExecuteSQLTask, Symmetrix.DataStage""
+            depends=""""
+            enabled=""True"">
+
+        <Property name=""ConnectionName"" value=""Stage""/>
+        <Property name=""SQLStatement"">
+<![CDATA[
+ 
+ set nocount on
+
+  ";
+
+
+
+
+
+
+    }
+}
